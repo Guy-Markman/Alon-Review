9cf32a4b106652c7560abf50e2e40a21a2534488
Guy Markman <dht1nre@hotmail.com>
2016-10-15 15:02:26 +0300

    Added SIGCHILD, fixed bugs, dup2 the right way

--- EX04/08-daemon.py
+++ EX04/08-daemon.py
@@ -15,9 +15,10 @@ def set_up():
     os.closerange(3, resource.RLIMIT_NOFILE)
     new_directory = os.open("/dev/null", os.O_RDWR)
     for x in range(3):
-        os.dup2(x, new_directory)
+        os.dup2(new_directory, x)
     os.chdir("/")
-    signal.signal(signal.SIGINT | signal.SIGTERM, terminate_handler)
+    signal.signal(signal.SIGINT, terminate_handler)
+    signal.signal(signal.SIGTERM, terminate_handler)
     signal.signal(signal.SIGHUP, signal.SIG_IGN)
 
 
@@ -25,7 +26,7 @@ def proc_child():
     counter = 0
     while True:
         time.sleep(1)
-        util.write_to_traget(1, counter)
+        util.write_to_target(1, str(counter))
         counter += 1
 
 
@@ -33,6 +34,7 @@ def main():
     set_up()
     child = os.fork()
     if child == 0:
+        signal.signal(signal.SIGUSR1, proc_child)
         proc_child()
     os._exit(0)
 
