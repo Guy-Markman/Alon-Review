a121fe1028dff23c5e49d4180356d9ad88b217bc
Water1234562 <dht1nre@hotmail.com>
2016-11-11 18:24:27 +0200

    Even more

--- EX05/ProxyServer.py
+++ EX05/ProxyServer.py
@@ -13,10 +13,12 @@ class ProxyServer(object):
         self.passive = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.passive.bind((host, bind_passive))
         self.passive.listen(1)
+        self.passive.setblocking(0)
         self.connection_list.append(self.passive)
 
         self.active = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.active.bind((host, bind_active))
+        self.passive.setblocking(0)
         self.connection_list.append(self.passive)
 
         self.poller = select.poll()
@@ -25,3 +27,8 @@ class ProxyServer(object):
         self.poller = select.poll()
         for s in self.connection_list:
             self.poller.register(s, READ_WRITE)
+
+    def proxy(self, args):
+        while True:
+            build_poller()
+            events = self.poller.poll()
