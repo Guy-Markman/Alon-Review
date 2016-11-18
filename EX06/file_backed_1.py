474bb3115d8919cf8ed9539a7e778152e85ad154
Guy Markman <dht1nre@hotmail.com>
2016-11-18 15:01:09 +0200

    Changes by Alon's review
    anonymous: changed hard code to variables
    file_backed_1: Removed unneeded contents, added constants
    file_backed_2: fixed with contextlib.closing
    
    All the files changed to LF

--- EX06/file_backed_1.py
+++ EX06/file_backed_1.py
@@ -6,6 +6,8 @@ import os
 import stat
 import time
 
+FILE_END = "\0"
+
 
 def main():
     fd = os.open(
@@ -13,13 +15,9 @@ def main():
         os.O_CREAT | os.O_RDWR,
         stat.S_IREAD | stat.S_IWRITE | stat.S_IRGRP | stat.S_IROTH
     )
-
-    os.lseek(fd, constants.FILE_SIZE-1, os.SEEK_SET)
-    before_changes = None
-    if os.fsta(fd).st_size >= constants.FILE_SIZE: # TODO: Change it so it'll work in both cases
-        os.lseek(fd, constants.START_LOCATION, os.SEEK_SET)
-        before_changes = os.read
-    os.write(fd, "\0")
+    while os.fstat(fd).st_size < constants.FILE_SIZE:
+        os.lseek(fd, constants.FILE_SIZE-1, os.SEEK_SET)
+        os.write(fd, FILE_END)
     try:
         with contextlib.closing(mmap.mmap(fd, constants.FILE_SIZE)) as mm:
             counter = 0
