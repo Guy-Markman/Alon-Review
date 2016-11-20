b2e525447303c476ddd70aae37b877d873634878
Water1234562 <dht1nre@hotmail.com>
2016-11-20 13:29:47 +0200

    Changes 20/11
    Fixed syntex and pep8

--- EX06/CyclicBuffer.py
+++ EX06/CyclicBuffer.py
@@ -1,43 +1,75 @@
 #!/usr/bin/env python
+import struct
+
 import base
 import constants
 
 
 class CyclicBuffer(base.Base):
 
-    def __init__(self, mm,):
+    def __init__(self, mm):
         super(CyclicBuffer, self).__init__()
+        self.logger.debug("Created cyclic Buffer")
         self.mm = mm
 
     def max_size_buffer(self):
         return constants.BUFFER_SIZE - 2 * constants.OFFSET_SIZE
 
     def get_head_offset(self):
-        return self.mm[-2 * constants.OFFSET_SIZE:-1 * constants.OFFSET_SIZE]
+        head_offset = self.mm[-2 *
+                              constants.OFFSET_SIZE:-1 * constants.OFFSET_SIZE]
+        self.logger.debug("head offset %s" % struct.unpack("b", head_offset))
+        return head_offset
 
     def set_head_offset(self, value):
+        self.logger.debug("Set head offset to %d" % value)
         self.mm[-2 * constants.OFFSET_SIZE:-1 * constants.OFFSET_SIZE] = value
 
     def get_tail_offset(self):
-        return self.mm[-1 * constants.OFFSET_SIZE:]
+        tail_offset = self.mm[-1 * constants.OFFSET_SIZE:]
+        self.logger.debug("Tail offset %d" % tail_offset)
+        return tail_offset
 
     def set_tail_offset(self, value):
+        self.logger.debug("Set tail offset to %d" % value)
         self.mm[-1 * constants.OFFSET_SIZE:] = value
 
     def read_head(self):
         head_offset = self.get_head_offset()
         ans = self.mm[head_offset]
-        if head_offset + 1 <= self.max_size_buffer():
-            self.set_head_offset(head_offset + 1)
-        else:
-            self.set_head_offset(0)
         return ans
 
     def read_tail(self):
-        tail_offset = self.get_head_offset()
+        tail_offset = self.get_tail_offset()
+        ans = self.mm[tail_offset]
+        return ans
+
+    def write_head(self, value):
+        head_offset = self.get_head_offset()
+        self.logger.debug("Wrote %d into %d" % (value, head_offset))
+        self.mm[head_offset] = value
+
+    def write_tail(self, value):
+        tail_offset = self.get_tail_offset()
+        self.logger.debug("Wrote %s into %d" % (value, tail_offset))
+        self.mm[tail_offset] = value
+
+    def increas_head(self):
+        head_offset = self.get_head_offset()
+        ans = self.mm[head_offset]
+        if head_offset + 1 <= self.max_size_buffer():
+            self.set_head_offset(head_offset + 1)
+            self.logger.debug("Increased head offset to" % (head_offset + 1))
+        else:
+            self.logger.debug("Reset head offset, returned %s" % ans)
+            self.set_head_offset(0)
+
+    def increas_tail(self):
+        tail_offset = self.get_tail_offset()
         ans = self.mm[tail_offset]
         if tail_offset + 1 <= self.max_size_buffer():
             self.set_tail_offset(tail_offset + 1)
+            self.logger.debug("Increased tail offset to" % (tail_offset + 1))
         else:
-            self.set_head_offset(0)
-        return ans
+            self.logger.debug("Reset tail offset, returned %s" % ans)
+            self.set_tail_offset(0)
---
+++ EX06/data_transfer.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+import argparse
+import contextlib
+import logging
+import mmap
+import os
+import random
+import struct
+import time
+
+import base
+import constants
+import CyclicBuffer
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        prog="package name",
+        description=("package"),
+    )
+    LOG_STR_LEVELS = {
+        'DEBUG': logging.DEBUG,
+        'INFO': logging.INFO,
+        'WARNING': logging.WARNING,
+        'ERROR': logging.ERROR,
+        'CRITICAL': logging.CRITICAL,
+    }
+    parser.add_argument(
+        '--log-level',
+        dest='log_level_str',
+        default='INFO',
+        choices=LOG_STR_LEVELS.keys(),
+        help='Log level',
+    )
+    parser.add_argument(
+        '--log-file',
+        dest='log_file',
+        metavar='FILE',
+        required=False,
+        help='Logfile to write to, otherwise will log to console.',
+    )
+    args = parser.parse_args()
+    args.log_level = LOG_STR_LEVELS[args.log_level_str]
+
+    logger = None
+
+    if args.log_file:
+        logger = base.setup_logging(
+            stream=open(args.log_file, 'a'),
+            level=args.log_level,
+        )
+    else:
+        logger = base.setup_logging(
+            level=args.log_level,
+        )
+    with contextlib.closing(mmap.mmap(-1, constants.BUFFER_SIZE)) as mm:
+        cb = CyclicBuffer.CyclicBuffer(mm)
+        cb.write_head(struct.pack("1"))
+        print cb.read_head()
+
+
+def main():
+    arg = parse_args()
+    logger.debug("check")
+
+if __name__ == "__main__":
+    main()
