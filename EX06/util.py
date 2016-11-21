47c183210dfd579bcb484fe7a56e11dbd3374554
Water1234562 <dht1nre@hotmail.com>
2016-11-21 14:34:38 +0200

    Util: data to int and reverse

--- EX06/util.py
+++ EX06/util.py
@@ -2,16 +2,31 @@
 import os
 import struct
 
+import constants
 
-def bytes_to_int(bytes):
-    ans = 0
-    for x in xrange(bytes):
-        ans += struct.unpack("B", x)[0]
-    return ans
+FORMAT = ("B", 16)
 
 
-def str_to_bytes(str):
-    ans =
+def data_to_int(data):
+    integer = 0
+    if len(data) == 0:
+        raise ValueError
+    for x in data:
+        integer = integer * FORMAT[1] + struct.unpack(FORMAT[0], x)[0]
+    return integer
+
+
+def int_to_data(integer):
+    data = ""
+    integer = "%0*x" % (2 * constants.OFFSET_SIZE, integer)
+    while integer:
+        data += struct.pack(FORMAT[0], int(integer[:2], FORMAT[1]))
+        integer = integer[2:]
+    return data
+
+
+def length_integer(integer):
+    return len(str(integer))
 
 
 def write_to_target(target, text):
